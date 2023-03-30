2bcc6f28f80ca8cf96be266063e5afb8a1e5f98e
Sauliha Shakeel <gangoosauliha@gmail.com>
2023-03-30 17:41:54 +0530

    E-Library Management System
    This Library Management System Project has been developed using PHP language with MySQL Database, JavaScript and Bootstrap 5 library. Under this Library Management System
    there are main two user, one is Admin or Librarian and Other is Student or User who will
    borrowed book from Library. In this Library Management System Admin or Librarian can maintain all data like add or manage category, authors, books, issued book returned book etc.
    Here Admin or Library is the key role in the management of this Library Management System
    Online.

---
+++ footer.php
@@ -0,0 +1,9 @@
+<style type="text/css">
+  #footer {
+    position : absolute;
+    bottom : -100;
+    height : 40px;
+    margin-top : 100px;
+  }
+</style>
+<div id="footer">&copy; Copyright 2022 <br>Developed by Sauliha & Basit Zaman</div>
